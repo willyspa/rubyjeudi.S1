e0ca22ee7dd261aed0ac3eb14b491f57b2939078
TheMindset <mulezig2014@outlook.fr>
2019-01-10 17:23:18 +0100

    ajout exo_02 ++

--- 02_password.rb
+++ 02_password.rb
@@ -1,40 +1,62 @@
-
 def signup
-	key = ' '
-
-	puts "Veuillez définir votre mots de passe"
-
-	
-	user_password = gets.chomp.to_s
-	key = user_password
-	 return key
-	
+	puts "choix de mot de passe : "
+	mdp = gets.chomp
+	return mdp
 end
 
+signup
 
 
 def login
-      	exit = false
+	puts "Rentrez votre mot de passe"
+	user_mdp = gets.chomp
+	return user_mdp
+end
 
-	while exit == false
 
-	  puts "Veuillez rentrer votre mot de passe ?"
-	  user_answer = gets.chomp.to_s
-		key = user_answer	
-	   if key == user_answer
-			
-		exit = true
-	  end
-        end
+login
+
+
+
+
+def signup
+    puts "Bonjour, initialise ton mot de passe"
+    print "> "
+    mdp = gets.chomp
+ return mdp
+end
+
+def trytry
+ puts "Mets ton mdp pour acceder à ton espace secret"
+ print "> "
+ try = gets.chomp
+ return try
+end
+
+def welcome_screen
+ puts "messages échangés avec HULK"
+ puts "Hulk : ça m'énerve"
+ puts "Hulk : calme-moi"
+end
+
+def login(signup, trytry)
+ mdp = signup
+ try = trytry
+
+while ( mdp != try )
+  puts "Tu t'es trompé, réessaie"
+  print "> "  
+  try = gets.chomp 
+   if try ==  mdp
+   end
+ end
+ ok = ok
 end
 
 def perform
-	
-	 signup 
-	 login
-	puts "Welcome to the NASA"
+    ok = login(signup, trytry)
+    welcome_screen
 end
 
 perform
-
 	
